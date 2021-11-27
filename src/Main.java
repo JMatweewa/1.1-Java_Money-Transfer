2f244e3256826fb3b5f7294dc5d797b052e91a77
JMatweewa <93136237+JMatweewa@users.noreply.github.com>
2021-11-27 16:54:53 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int amountToTransfer = 500000000;
+        int totalBalance = balance + amountToTransfer;
+        System.out.println(totalBalance);
+    }
+}
